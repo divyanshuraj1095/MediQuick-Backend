73062ff8e9358be02c7b6208b7e8adcad3132fe3
Divyanshu Raj <divyanshuraj23102005@gmail.com>
2025-12-18 19:46:44 +0530

    pharmacy schema created

--- backend/models/Pharmacy.js
+++ backend/models/Pharmacy.js
@@ -10,8 +10,38 @@ const pharmacySchema = new mongoose.Schema(
         phone : {
             type : String,
             required : [true, "phone number is required!!"],
-            
+        },
+        owner : {
+            type : mongoose.Schema.Types.ObjectId,
+            ref : "User",
+            required : true,
+        },
+        location : {
+            type : {
+                type : String,
+                enum : ["Point"],
+                default : "Point",
+            },
+            coordinates : {
+                type : [Number],
+                required : true,
+            },
+        },
+        isOpen : {
+            type : Boolean,
+            default : true,
+        },
+        deliveryRadiusKm : {
+            type : Number,
+            default : 5,
+        },
+        createdAt : {
+            type : Date,
+            default : Date.now,
+        },
+    },
+    {timestamps : true}
+);
 
-        }
-    }
-)
+pharmacySchema.index({location : "2dsphere"});
+module.exports = mongoose.model("Pharmacy", pharmacySchema);
